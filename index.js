730227d00b82c2650b4c2979efdd7738dd1ef918
Alexander Bazo <alexanderbazo@googlemail.com>
2021-04-26 13:55:45 +0200

    Add generator for vp sheets

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 
 import express from "express";
 
+import stream from "stream";
+
 import Config from "./lib/Config.js";
 import VPProvider from "./lib/VPProvider.js";
+import FormGenerator from "./lib/FormGenerator.js";
 
 var appConfig,
     provider,
+    generator,
     app; // express
 
 async function handleVPRequest(req, res) {
@@ -15,16 +19,34 @@ async function handleVPRequest(req, res) {
     res.json(results);
 }
 
+async function handleFormRequest(req, res) {
+    let formFormBuffer = await generator.getFormBufferFor({
+            title: "Remote-Studie zum Coding-Verhalten",
+            researcher: "Alexander Bazo",
+            context: "Abschlussarbeit",
+            supervisor: "Alexander Bazo (Supervisor)",
+            chair: "Medieninformatik",
+            vps: "1"
+        }),
+        readStream = new stream.PassThrough();
+    readStream.end(formFormBuffer);
+    res.set("Content-disposition", "attachment; filename=" + "VP-Liste.xlsx");
+    res.set("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+    readStream.pipe(res);
+}
+
 function setupExpress(port) {
     app = express();
     app.use(express.static("app"));
     app.get("/:id/vps", handleVPRequest);
+    app.get("/generator", handleFormRequest);
     app.listen(port);
 }
 
 function run(port) {
     appConfig = new Config();
     provider = new VPProvider(appConfig);
+    generator = new FormGenerator(appConfig);
     setupExpress(port);
 }
 
